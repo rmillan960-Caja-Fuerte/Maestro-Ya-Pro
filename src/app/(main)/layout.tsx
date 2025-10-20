4b546e360a0256be1ed1cff17562dadf4388c13a
Rafael Millan <rmillan960@gmail.com>
2025-10-20 01:14:48 +0000

    primero corrige lo de la pantalla  que esta como divida en 2 o no se rev

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -4,11 +4,11 @@ import AppSidebar from '@/components/layout/sidebar';
 
 export default function MainLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex min-h-screen w-full flex-col bg-muted/40">
+    <div className="flex min-h-screen w-full bg-muted/40">
       <AppSidebar />
-      <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
+      <div className="flex flex-col flex-1 sm:pl-14">
         <AppHeader />
-        <main className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8">
+        <main className="flex-1 p-4 sm:px-6 sm:py-6">
           {children}
         </main>
       </div>
