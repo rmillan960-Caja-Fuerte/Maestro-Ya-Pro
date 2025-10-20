c33aff7023d43472e5a5bfa94db2c7e4775b3d9a
Rafael Millan <rmillan960@gmail.com>
2025-10-20 02:06:57 +0000

    continua

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import MainLayout from './(main)/layout';
+import DashboardPage from './(main)/page';
+
+export default function Home() {
+  return (
+    <MainLayout>
+      <DashboardPage />
+    </MainLayout>
+  );
+}
