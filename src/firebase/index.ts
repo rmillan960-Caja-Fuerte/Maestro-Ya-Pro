221421a9e77c2354e5c5c59e4aab162fe32636b6
Rafael Millan <rmillan960@gmail.com>
2025-10-20 12:51:02 +0000

    VM461 node_modules_n…_8f19e6fb._.js:1226 Error with Google sign in: Fire

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -2,8 +2,8 @@
 
 import { firebaseConfig } from '@/firebase/config';
 import { initializeApp, getApps, getApp, FirebaseApp } from 'firebase/app';
-import { getAuth, connectAuthEmulator } from 'firebase/auth';
-import { getFirestore, connectFirestoreEmulator } from 'firebase/firestore'
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore'
 
 // IMPORTANT: DO NOT MODIFY THIS FUNCTION
 export function initializeFirebase() {
@@ -34,22 +34,6 @@ export function initializeFirebase() {
 export function getSdks(firebaseApp: FirebaseApp) {
   const firestore = getFirestore(firebaseApp);
   const auth = getAuth(firebaseApp);
-
-  if (process.env.NODE_ENV === 'development') {
-    try {
-      // Check if emulators are already running to avoid errors on hot-reloads
-      // @ts-ignore
-      if (!firestore._settings.host) {
-        connectFirestoreEmulator(firestore, 'localhost', 8080);
-      }
-      // @ts-ignore
-      if (!auth.emulatorConfig) {
-        connectAuthEmulator(auth, 'http://localhost:9099', { disableWarnings: true });
-      }
-    } catch (e) {
-        console.error('Error connecting to Firebase Emulators. Make sure they are running.', e);
-    }
-  }
 
   return {
     firebaseApp,
